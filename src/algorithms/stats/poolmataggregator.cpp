02ed0c46470d5d586806fa1259d9f45d752405c7
Arnold Kalmbach <akalmbach@cim.mcgill.ca>
2013-11-08 17:18:48 -0500

    Modified to actually fill in the output

--- src/algorithms/stats/poolmataggregator.cpp
+++ src/algorithms/stats/poolmataggregator.cpp
@@ -41,11 +41,18 @@ void PoolMatAggregator::compute() {
             // Get value in input(_field) as a vector< vector<Real> >
             vector< vector<Real> > input_data = input.value< vector <vector<Real> > >(_field);
             // Initialize output to num rows = outer vector size, num cols = inner vector size
-            // TODO: verify that type = CV_32FC1 is the way all the ml algorithms take their input
-	    //output.zeros(100, 100, CV_32FC1);
+            int rows = input_data.size();
+            int cols = input_data[0].size();
+            output(rows, cols, CV_32FC1);
         
             // Iterate over the elements, cast them as float
             // and put them in the right place in the output
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cols; c++) {
+                    // TODO: Handle badly formatted fields where this will go out of range!
+                    output.at<float>(r, c) = (float) input_data[r][c];
+                }
+            }
         }
         else {
             // TODO: Throw an exception of some kind
