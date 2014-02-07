8f729f48d15441d80a22f9c0dc78df8ef4384732
Arnold Kalmbach <akalmbach@cim.mcgill.ca>
2014-02-07 15:55:42 -0500

    Made path relative for running audio_server script

--- utils/audio_server.sh
+++ utils/audio_server.sh
@@ -6,7 +6,7 @@ gst-launch -v souphttpsrc location=http://stream.ckut.ca:8000/903fm-128-stereo !
 jack_disconnect gst-launch-0.10:out_jackaudiosink0_1 system:playback_1
 jack_disconnect gst-launch-0.10:out_jackaudiosink0_2 system:playback_2
 
-/home/arnold/Projects/essentia/build/src/rtwords/genVocab &
+./build/src/rtwords/genVocab &
 jack_disconnect essentia:output system:playback_1
 
 jack_connect gst-launch-0.10:out_jackaudiosink0_1 essentia:input
