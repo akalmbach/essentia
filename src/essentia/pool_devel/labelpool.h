2753a697f8a704ea7125950aaa8811a72807a2e7
Arnold Kalmbach <akalmbach@cim.mcgill.ca>
2014-03-12 17:28:12 -0400

    Created a few files for work on the new hierarchy of pools

---
+++ src/essentia/pool_devel/labelpool.h
@@ -0,0 +1,15 @@
+#include "rtpool.h"
+#include "jsonxx"
+
+#ifndef ESSENTIA_LABELPOOL_H
+#define ESSENTIA_LABELPOOL_H
+
+namespace essentia {
+
+class LabelPool : public RTPool {
+
+};
+
+}
+
+#endif //ESSENTIA_LABELPOOL_H
---
+++ src/essentia/pool_devel/rtpool.h
@@ -0,0 +1,14 @@
+#include "pool.h"
+
+#ifndef ESSENTIA_RTPOOL_H
+#define ESSENTIA_RTPOOL_H
+
+namespace essentia {
+
+class RTPool : Pool
+
+};
+
+}
+
+#endif //ESSENTIA_RTPOOL_H
